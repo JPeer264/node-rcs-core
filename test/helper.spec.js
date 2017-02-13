db8ca9d31fd8c5c274846782d2cf3e0e78f3e72a
JPeer264 <jan.oster94@gmail.com>
2017-02-13 12:56:52 +0100

    add helper.saveSync

--- test/helper.spec.js
+++ test/helper.spec.js
@@ -13,54 +13,66 @@ describe('helper.js', () => {
         fs.removeSync(testCwd);
     });
 
-    it('should create a file within a non existing dir', done => {
-        const filePath = path.join(testCwd, '/a/non/existing/path/test.txt');
+    describe('save', () => {
+        it('should create a file within a non existing dir', done => {
+            const filePath = path.join(testCwd, '/a/non/existing/path/test.txt');
 
-        rcs.helper.save(filePath, 'test content', (err, data) => {
-            expect(err).to.not.exist;
+            rcs.helper.save(filePath, 'test content', (err, data) => {
+                expect(err).to.not.exist;
 
-            expect(fs.existsSync(filePath)).to.be.true;
+                expect(fs.existsSync(filePath)).to.be.true;
+                expect(fs.readFileSync(filePath, 'utf8')).to.equal('test content');
+
+                done();
+            });
+        });
+
+        it('should not overwrite the same file', done => {
+            const filePath = path.join(testCwd, '/../config.json');
+            const oldFile = fs.readFileSync(filePath, 'utf8');
+
+            rcs.helper.save(filePath, 'test content', err => {
+                expect(err.message).to.equal('File exist and cannot be overwritten. Set the option overwrite to true to overwrite files.');
+                expect(fs.readFileSync(filePath, 'utf8')).to.equal(oldFile);
+
+                done();
+            });
+        });
+
+        it('should save SYNC', () => {
+            const filePath = path.join(testCwd, '/config.txt');
+
+            rcs.helper.saveSync(filePath, 'test content');
+
             expect(fs.readFileSync(filePath, 'utf8')).to.equal('test content');
-
-            done();
         });
-    });
 
-    it('should not overwrite the same file', done => {
-        const filePath = path.join(testCwd, '/../config.json');
-        const oldFile = fs.readFileSync(filePath, 'utf8');
+        it('should not overwrite the same file SYNC', () => {
+            const filePath = path.join(testCwd, '/../config.json');
+            const oldFile = fs.readFileSync(filePath, 'utf8');
 
-        rcs.helper.save(filePath, 'test content', err => {
-            expect(err.message).to.equal('File exist and cannot be overwritten. Set the option overwrite to true to overwrite files.');
+            try {
+                rcs.helper.saveSync(filePath, 'test content');
+
+                // if no error is thrown before it should fail here
+                expect(false).to.be.true;
+            } catch (e) {
+                expect(e.message).to.equal('File exist and cannot be overwritten. Set the option overwrite to true to overwrite files.');
+            }
+
             expect(fs.readFileSync(filePath, 'utf8')).to.equal(oldFile);
-
-            done();
         });
     });
 
-    it('should generatea readable json string from a json object', done => {
-        const object = { a: 1, b:2, c:3 };
-        const jsonString = rcs.helper.objectToJson(object);
+    describe('objectToJson', () => {
+        it('should generatea readable json string from a json object', done => {
+            const object = { a: 1, b:2, c:3 };
+            const jsonString = rcs.helper.objectToJson(object);
 
-        expect(object).to.be.a('object');
-        expect(jsonString).to.be.a('string');
+            expect(object).to.be.a('object');
+            expect(jsonString).to.be.a('string');
 
-        done();
-    });
-
-    it('should read a json file and turn it to an object', done => {
-        const obj = json.readToObjSync('test/files/config.json');
-
-        expect(obj).to.be.an('object');
-
-        done();
-    });
-
-    it('should fail to read a file', done => {
-        const obj = json.readToObjSync('not/existing/path/config.json');
-
-        expect(obj).to.be.false;
-
-        done();
+            done();
+        });
     });
 });
