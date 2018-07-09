c7efd4e74283fc4b585e6749c6218c76118ab1ea
Jan Peer Stöcklmair <jan.oster94@gmail.com>
2018-07-09 12:08:57 +0200

    Feat: replace HTML (closes #50) (#52)
    Todo: add an option for other attributes.

--- examples/replace/index.js
+++ examples/replace/index.js
@@ -7,7 +7,7 @@ rcs.fillLibraries(fs.readFileSync('./before/bootstrap.css', 'utf8'));
 // replace the data
 const cssData = rcs.replace.css(fs.readFileSync('./before/bootstrap.css', 'utf8'));
 const jsData = rcs.replace.js(fs.readFileSync('./before/bootstrap.js', 'utf8'));
-const htmlData = rcs.replace.any(fs.readFileSync('./before/index.html', 'utf8'));
+const htmlData = rcs.replace.html(fs.readFileSync('./before/index.html', 'utf8'));
 
 // write new files
 fs.writeFileSync('./after/bootstrap.replaced.css', cssData);
