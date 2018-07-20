3a32774a69adf72f5f18dfa16a516671dbcf3f48
JPeer264 <jan.oster94@gmail.com>
2018-07-20 14:16:18 +0200

    Feat: add counts

--- docs/api/stats.md
+++ docs/api/stats.md
@@ -3,6 +3,8 @@
 > Some statistics to analyze your code
 
 Returns object with:
+- classUsageCount `<Object>`: Key is the class and value is how often this selector got used
+- idUsageCount `<Object>`: Same as `classUsageCount`, but for ids
 - unsusedClasses `<String[]>`: A list of unused classes
 - unsusedIds `<String[]>`: A list of unused ids
 
--- test/stats.js
+++ test/stats.js
@@ -16,12 +16,14 @@ test('replace js and get correct classes', (t) => {
 
 test('replace js and get correct classes and ids', (t) => {
   rcs.selectorLibrary.fillLibrary('#id {} .selector {} .not-used {} .used {}');
-  rcs.replace.js('var a = \'selector used\';');
+  rcs.replace.js('var a = "selector used";');
 
   const stats = rcs.stats();
 
   t.deepEqual(stats.unusedClasses, ['not-used']);
   t.deepEqual(stats.unusedIds, ['id']);
+  t.deepEqual(stats.classUsageCount, { 'not-used': 0, used: 1, selector: 1 });
+  t.deepEqual(stats.idUsageCount, { id: 0 });
 });
 
 // following should pass after issue #51 is resolved
@@ -33,6 +35,8 @@ test.skip('replace html and get correct classes and ids', (t) => {
 
   t.deepEqual(stats.unusedClasses, ['not-used']);
   t.deepEqual(stats.unusedIds, ['id']);
+  t.deepEqual(stats.classUsageCount, { 'not-used': 0, used: 1, selector: 1 });
+  t.deepEqual(stats.idUsageCount, { id: 0 });
 });
 
 test('replace html and get correct classes and ids', (t) => {
@@ -43,6 +47,8 @@ test('replace html and get correct classes and ids', (t) => {
 
   t.deepEqual(stats.unusedClasses, ['not-used']);
   t.deepEqual(stats.unusedIds, ['id']);
+  t.deepEqual(stats.classUsageCount, { 'not-used': 0, used: 1, selector: 1 });
+  t.deepEqual(stats.idUsageCount, { id: 0 });
 });
 
 test('replace css and get correct classes and ids', (t) => {
@@ -53,4 +59,20 @@ test('replace css and get correct classes and ids', (t) => {
 
   t.deepEqual(stats.unusedClasses, ['not-used']);
   t.deepEqual(stats.unusedIds, []);
+  t.deepEqual(stats.classUsageCount, { 'not-used': 0, used: 1, selector: 1 });
+  t.deepEqual(stats.idUsageCount, { id: 1 });
+});
+
+test('replace all and get correct classes and ids', (t) => {
+  rcs.selectorLibrary.fillLibrary('#id {} .selector {} .not-used {} .used {}');
+  rcs.replace.css('#id {} .selector {} .used {}');
+  rcs.replace.html('<div class="selector used"></div>');
+  rcs.replace.js('var a = "used";');
+
+  const stats = rcs.stats();
+
+  t.deepEqual(stats.unusedClasses, ['not-used']);
+  t.deepEqual(stats.unusedIds, []);
+  t.deepEqual(stats.classUsageCount, { 'not-used': 0, used: 3, selector: 2 });
+  t.deepEqual(stats.idUsageCount, { id: 1 });
 });
