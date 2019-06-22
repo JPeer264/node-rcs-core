a5455a0d74e51a903244d3e0fe73252e6698efd4
JPeer264 <jan.oster94@gmail.com>
2019-06-22 16:01:22 +0200

    Feat: extract value from html tags

---
+++ test/helpers/extractFromHtml.js
@@ -0,0 +1,33 @@
+import test from 'ava';
+
+import rcs from '../../lib';
+
+test('should extract nothing', (t) => {
+  const { extractFromHtml } = rcs.helpers;
+
+  const extracted = extractFromHtml('<!DOCTYPE html><html><head></head><body>Hi there!</body></html>');
+  const extractedSecond = extractFromHtml('');
+
+  t.is(extracted.length, 0);
+  t.is(extractedSecond.length, 0);
+});
+
+test('should extract style tags', (t) => {
+  const { extractFromHtml } = rcs.helpers;
+
+  const extractedStyle = extractFromHtml('<!DOCTYPE html><html><head></head><body>Hi there!<style>.test{} .css{}</style></body></html>');
+
+  t.is(extractedStyle.length, 1);
+  t.is(extractedStyle[0], '.test{} .css{}');
+});
+
+test('should extract script tags', (t) => {
+  const { extractFromHtml } = rcs.helpers;
+
+  const extractedScript = extractFromHtml('<!DOCTYPE html><html><head></head><body>Hi there!<script type="text/javascript">var test = 123;</script></body></html>', 'script');
+  const extractedStyle = extractFromHtml('<!DOCTYPE html><html><head></head><body>Hi there!<script type="text/javascript">var test = 123;</script></body></html>');
+
+  t.is(extractedScript.length, 1);
+  t.is(extractedStyle.length, 0);
+  t.is(extractedScript[0], 'var test = 123;');
+});
