dcb3ac3e3fe2ea8f3ad8145bd2f1ffcec4ab2136
JPeer264 <jan.oster94@gmail.com>
2019-09-30 11:05:57 +0200

    Chore: add typescript eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
+    env: {
+        browser: true,
+        es6: true,
         jest: true,
     },
-    "extends": "airbnb-base",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'airbnb-base'
+    ],
+    plugins: ['@typescript-eslint'],
+    parser: '@typescript-eslint/parser',
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    "rules": {
+    rules: {
+        '@typescript-eslint/explicit-function-return-type': ['warn', {
+            allowTypedFunctionExpressions: true,
+            allowExpressions: true,
+        }],
     }
 };
